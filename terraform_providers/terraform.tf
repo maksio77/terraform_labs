a80971c5682b5fbca6b7ef4ad2c1e5bbcc8fb640
Максим Павлів <maksimpavliv@MacBook-Pro-Maksim.local>
2025-08-29 18:02:07 +0300

    providers block

---
+++ terraform_providers/terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
